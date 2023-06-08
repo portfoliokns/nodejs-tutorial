b5322631d37103d72e1ee77ae19505d94f1f24c6
konishi333 <jobhuntingkonishi3@gmail.com>
2023-06-08 16:25:48 +0900

    補足事項などを修正

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+//初期設定
 const http = require("http");
 const PORT = 8000
 const html = require("fs").readFileSync("./index.html")
 
-//webサーバーを作る
+//webサーバーを構築
 const server = http.createServer((req,res) => {
   //ブラウザからアクセスがきた時の処理
   res.writeHead(200, {"Content-Type": "text/html"});
@@ -10,14 +11,14 @@ const server = http.createServer((req,res) => {
   res.write(html);
   res.end();
 
+  // HTTPメソッドの種類に応じて、処理を変更することができる
   if (req.method == "GET"){
-
   }
   if (req.method == "POST"){
-
   }
 });
 
+// ローカル環境での実行
 server.listen(PORT, () => {
   console.log('server running')
 });
